f47ba5e0a139ff59df09aa3f37bff468d6d63664
Rahul Krishna <rahul.krishna8590@gmail.com>
2021-01-05 22:56:28 +0530

    initial commit
    Signed-off-by: Rahul Krishna <rahul.krishna8590@gmail.com>

---
+++ react_project/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+       <h3>Student CRUD</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
